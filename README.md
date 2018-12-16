f5dbccc92cc7a7dfb0824c673e8240763b62b638
Rafael Valiev <morsesfeeler@live.ru>
2018-12-16 18:57:52 +0300

    SimpleLogic for dictionary

--- README.md
+++ README.md
@@ -18,3 +18,7 @@ Jeg lære Norsk. Jeg har ikke nyttig web ordbok. Jeg vil skape ordboken selv.
 * [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 * [Build Chrome extention with ReactJs](https://medium.com/@gilfink/building-a-chrome-extension-using-react-c5bfe45aaf36)
 * [Building extensions with Grammarly](https://tech.grammarly.com/blog/building-browser-extensions-at-scale)
+* [Interactive extension](https://blog.usejournal.com/making-an-interactive-chrome-extension-with-react-524483d7aa5d)
+https://robots.thoughtbot.com/redux-for-chrome-extensions
+https://developer.chrome.com/extensions/devguide
+https://medium.freecodecamp.org/building-chrome-extensions-in-react-parcel-79d0240dd58f
--- src/App.js
+++ src/App.js
@@ -1,61 +1,25 @@
-/* global chrome */
-
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      domain: '',
-      headlines: []
-    };
-  }
-  componentDidMount() {
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      const url = new URL(tabs[0].url);
-      const domain = url.hostname;
-      this.setState({
-        domain: domain
-      });
-      this.getHeadlines(domain);
-    });
-  }
-  getHeadlines(query) {
-    axios
-      .get('https://newsapi.org/v2/everything', {
-        params: {
-          q: query,
-          language: 'en',
-          apiKey: '06e04921b3744045bdb5ae1b4463c522'
-        }
-      })
-      .then(results => {
-        this.setState({
-          headlines: results.data.articles.slice(0, 5)
-        });
-      })
-      .catch(error => {
-        console.log('Error in obtaining headlines', error);
-      });
-  }
   render() {
     return (
       <div className="App">
-        <h1 className="App-title">{this.state.domain}</h1>
-        Top Headlines:
-        {this.state.headlines.map(headline => (
-          <h4
-            className="link"
-            onClick={() => {
-              window.open(headline.url);
-            }}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            {headline.title}
-          </h4>
-        ))}
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
