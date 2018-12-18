ab5ef30530737b794545b83e5054c478cbdb05b9
Валиев Рафаэль Рамилевич <Valiev.R.Ra@sberbank.ru>
2018-12-18 12:56:27 +0300

    Moved front to dir

--- google-extension/src/store.js
+++ google-extension/src/store.js
@@ -3,7 +3,7 @@ import { applyMiddleware, createStore } from "redux";
 import { logger } from "redux-logger";
 import thunk from "redux-thunk";
 import promise from "redux-promise-middleware";
-import reducer from "./reducers";
+import reducer from "./reducers/Index";
 
 const middleware = applyMiddleware(promise(), thunk, logger);
 
